45a9d00401b07506a236d3714dde06220f700d9c
darkangel <darkangelba@gmail.com>
2016-07-27 18:01:43 -0300

    - initial commit

---
+++ SqlToJson/Main.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace SqlToJson
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txConnStr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txSqlQuery = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txOutFile = new System.Windows.Forms.TextBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lvPreview = new System.Windows.Forms.ListView();
+            this.lblRowCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txConnStr
+            // 
+            this.txConnStr.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txConnStr.Location = new System.Drawing.Point(106, 12);
+            this.txConnStr.Name = "txConnStr";
+            this.txConnStr.Size = new System.Drawing.Size(395, 20);
+            this.txConnStr.TabIndex = 0;
+            this.txConnStr.Text = "Data Source=localhost;Initial Catalog=argentina;Integrated Security=true";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Connection String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SQL Query";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txSqlQuery
+            // 
+            this.txSqlQuery.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txSqlQuery.Location = new System.Drawing.Point(106, 38);
+            this.txSqlQuery.Name = "txSqlQuery";
+            this.txSqlQuery.Size = new System.Drawing.Size(395, 20);
+            this.txSqlQuery.TabIndex = 2;
+            this.txSqlQuery.Text = "SELECT TOP 100 * FROM [regions]";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Output File";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txOutFile
+            // 
+            this.txOutFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txOutFile.Location = new System.Drawing.Point(106, 64);
+            this.txOutFile.Name = "txOutFile";
+            this.txOutFile.Size = new System.Drawing.Size(395, 20);
+            this.txOutFile.TabIndex = 4;
+            this.txOutFile.Text = "C:\\Test\\jsonResults.json";
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPreview.Location = new System.Drawing.Point(345, 229);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(75, 23);
+            this.btnPreview.TabIndex = 6;
+            this.btnPreview.Text = "Preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(426, 229);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lvPreview
+            // 
+            this.lvPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvPreview.GridLines = true;
+            this.lvPreview.Location = new System.Drawing.Point(12, 91);
+            this.lvPreview.Name = "lvPreview";
+            this.lvPreview.Size = new System.Drawing.Size(489, 132);
+            this.lvPreview.TabIndex = 7;
+            this.lvPreview.UseCompatibleStateImageBehavior = false;
+            this.lvPreview.View = System.Windows.Forms.View.Details;
+            // 
+            // lblRowCount
+            // 
+            this.lblRowCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblRowCount.AutoSize = true;
+            this.lblRowCount.Location = new System.Drawing.Point(12, 229);
+            this.lblRowCount.Name = "lblRowCount";
+            this.lblRowCount.Size = new System.Drawing.Size(0, 13);
+            this.lblRowCount.TabIndex = 8;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 260);
+            this.Controls.Add(this.lblRowCount);
+            this.Controls.Add(this.lvPreview);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txOutFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txSqlQuery);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txConnStr);
+            this.Name = "Main";
+            this.Text = "SQL to Json";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txConnStr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txSqlQuery;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txOutFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListView lvPreview;
+        private System.Windows.Forms.Label lblRowCount;
+    }
+}
+
---
+++ SqlToJson/Main.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace SqlToJson
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private DataTable sqlData = new DataTable();
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            btnSave.Enabled = false;
+            try
+            {
+                lvPreview.Items.Clear();
+                lvPreview.Columns.Clear();
+
+                FetchSqlData();
+            }
+            catch (Exception ex)
+            {
+                lblRowCount.Text = string.Format("ERROR: {0}", ex.Message);
+            }
+            this.Enabled = true;
+        }
+
+
+        public void FetchSqlData()
+        {
+            sqlData = new DataTable();
+            using (SqlConnection conn = new SqlConnection(txConnStr.Text))
+            {
+                using (SqlCommand cmd = new SqlCommand(txSqlQuery.Text, conn))
+                {
+                    conn.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(sqlData);
+                    PopulateListViewPreview(sqlData);
+                }
+            }
+        }
+
+        private void PopulateListViewPreview(DataTable data) {
+            lvPreview.Items.Clear();
+            foreach (DataColumn column in data.Columns)
+            {
+                var col = lvPreview.Columns.Add(column.ColumnName);
+            }
+            foreach (DataRow row in data.Rows)
+            {
+                ListViewItem item = new ListViewItem(row[0].ToString());
+                for (int i = 1; i < data.Columns.Count; i++)
+                    item.SubItems.Add(row[i].ToString());
+                lvPreview.Items.Add(item);
+            }
+            lvPreview.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            lblRowCount.Text = string.Format("{0} results", data.Rows.Count.ToString());
+            btnSave.Enabled = data.Rows.Count > 0;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (sqlData.Rows.Count == 0) {
+                lblRowCount.Text = "no data to serialize!!";
+                return;
+            }
+            string jsonResultStr = JsonConvert.SerializeObject(sqlData, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore });
+            System.IO.File.WriteAllText(txOutFile.Text, jsonResultStr);
+            lblRowCount.Text = string.Format("{0} items saved in {1}", sqlData.Rows.Count.ToString(), txOutFile.Text);
+        }
+
+    }
+}
